88ad3d0c8d8ccf7786e3700e447e0cbfb21f05a3
ingalls <nicholas.ingalls@gmail.com>
2015-08-06 12:25:04 -0400

    don't reprocess

--- make.sh
+++ make.sh
@@ -20,6 +20,11 @@ while read -r LINE; do
     CENTRE=$(echo $LINE | jq -r -c '.geometry | .coordinates' | sed -e 's/\[//' -e 's/]//')
     PID=$(echo $LINE | jq -r -c '.properties | .WEB_PUBLIC' | sed -e 's/^.*=//' -e 's/\-//g' -e 's/\.//g')
 
+    if [ ! -z $(grep -E "${PID}$" $(dirname $0)/out.csv ) ]; then
+        echo "$PIN already processed"
+        continue
+    fi
+
     WEB=$(curl -s "http://www.sumner.kansasgis.com/ParcelDetail.aspx?parcel=${PID}&quickref=R2"  -H 'Host: www.sumner.kansasgis.com' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'DNT: 1' -H 'Cookie: __utma=165988668.91698580.1438486744.1438856372.1438870996.3; __utmz=165988668.1438486744.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); ASP.NET_SessionId=kcxedwhm0ywdqomwfuuzmux0; __utmc=165988668; __utmb=165988668.11.10.1438870996; __utmt=1' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0')
 
     if [ -z $(echo $WEB | grep "GeneralInfo_content_gvwPropertySitusInfo_lblSitusAddress_0") ]; then
